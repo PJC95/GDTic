675f16e03c86d32b9489f7d3b9e323d3700885a0
Paul Connolly <pjconnolly1995@gmail.com>
2024-08-12 14:38:29 +0100

    Initial commit

---
+++ scripts/choose_side.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _on_choice_x_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_choice_o_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	
+func _on_choice_x_mouse_entered():
+	$ChoiceX.grab_focus()
+
+func _on_choice_o_mouse_entered():
+	$ChoiceO.grab_focus()
+
+func _on_choice_x_mouse_exited():
+	$ChoiceX.release_focus()
+	
+func _on_choice_o_mouse_exited():
+	$ChoiceO.release_focus()
