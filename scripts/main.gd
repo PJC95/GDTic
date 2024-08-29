120fed52810b0f681df10bd90e7498d51f79ef73
Paul Connolly <pjconnolly1995@gmail.com>
2024-08-29 19:42:50 +0100

    Two different symbols can no longer be placed in the same place on the grid

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+@onready var array_x: Array = [$Xs/X, $Xs/X2, $Xs/X3, $Xs/X4, $Xs/X5, $Xs/X6, $Xs/X7, $Xs/X8, $Xs/X9]
+@onready var array_o: Array = [$Os/O, $Os/O2, $Os/O3, $Os/O4, $Os/O5, $Os/O6, $Os/O7, $Os/O8, $Os/O9]
+
+func _process(_delta) -> void:
+	for i in range(len(array_x)):
+		if array_x[i].is_placed == true:
+			array_o[i].can_be_placed = false
+			
+	for i in range(len(array_o)):
+		if array_o[i].is_placed == true:
+			array_x[i].can_be_placed = false
+			
 # func _process(delta):
 	# var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	# print(mouse_position)
--- scripts/o.gd
+++ scripts/o.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var sprite_2d: Sprite2D = $Area2D/Sprite2D
 @onready var timer: Timer = $Timer
+@onready var is_placed: bool = false
+@onready var can_be_placed: bool = true
 
 var left_mouse_pressed: bool = false
 var mouse_in_area: bool = false
@@ -18,8 +20,9 @@ func _input(InputEvent) -> void:
 			
 # If cursor is in node's area, left mouse is pressed and it's O's turn, make the sprite visible
 func _process(_delta) -> void:
-	if mouse_in_area and left_mouse_pressed and GameManager.turn == "O":
+	if mouse_in_area and left_mouse_pressed and GameManager.turn == "O" and can_be_placed == true:
 		sprite_2d.visible = true
+		is_placed = true
 		timer.start()
 		
 func _on_area_2d_mouse_entered() -> void: 
