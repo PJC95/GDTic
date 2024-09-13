35021269959b368b32cdd3b172ea312f14fd6bee
PJC95 <pjconnolly95@pm.me>
2024-09-13 17:20:53 +0100

    Refactored win condition code to use lookup table

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -26,53 +26,22 @@ func check_if_won() -> void:
 	$"../Main/Xs/X5", $"../Main/Xs/X6", $"../Main/Xs/X7", $"../Main/Xs/X8", $"../Main/Xs/X9"]
 	var array_o: Array = [$"../Main/Os/O", $"../Main/Os/O2", $"../Main/Os/O3", $"../Main/Os/O4", 
 	$"../Main/Os/O5", $"../Main/Os/O6", $"../Main/Os/O7", $"../Main/Os/O8", $"../Main/Os/O9"]
-
-	# X win conditions
-	if array_x[0].is_placed and array_x[1].is_placed and array_x[2].is_placed:
-		print(x_win)
-		
-	elif array_x[3].is_placed and array_x[4].is_placed and array_x[5].is_placed:
-		print(x_win)
-		
-	elif array_x[6].is_placed and array_x[7].is_placed and array_x[8].is_placed:
-		print(x_win)
-		
-	elif array_x[0].is_placed and array_x[3].is_placed and array_x[6].is_placed:
-		print(x_win)
-		
-	elif array_x[1].is_placed and array_x[4].is_placed and array_x[7].is_placed:
-		print(x_win)
-		
-	elif array_x[2].is_placed and array_x[5].is_placed and array_x[8].is_placed:
-		print(x_win)
-		
-	elif array_x[0].is_placed and array_x[4].is_placed and array_x[8].is_placed:
-		print(x_win)
-		
-	elif array_x[2].is_placed and array_x[4].is_placed and array_x[6].is_placed:
-		print(x_win)
-
-	# O win conditions
-	elif array_o[0].is_placed and array_o[1].is_placed and array_o[2].is_placed:
-		print(o_win)
-		
-	elif array_o[3].is_placed and array_o[4].is_placed and array_o[5].is_placed:
-		print(o_win)
-		
-	elif array_o[6].is_placed and array_o[7].is_placed and array_o[8].is_placed:
-		print(o_win)
-		
-	elif array_o[0].is_placed and array_o[3].is_placed and array_o[6].is_placed:
-		print(o_win)
-		
-	elif array_o[1].is_placed and array_o[4].is_placed and array_o[7].is_placed:
-		print(o_win)
-		
-	elif array_o[2].is_placed and array_o[5].is_placed and array_o[8].is_placed:
-		print(o_win)
-		
-	elif array_o[0].is_placed and array_o[4].is_placed and array_o[8].is_placed:
-		print(o_win)
-		
-	elif array_o[2].is_placed and array_o[4].is_placed and array_o[6].is_placed:
-		print(o_win)
+	
+	# All possible win conditions (as indices in the array)
+	var win_conditions: Array = [
+		[0, 1, 2], [3, 4, 5], [6, 7, 8],	# Horizontal
+		[0, 3, 6], [1, 4, 7], [2, 5, 8],	# Vertical
+		[0, 4, 8], [2, 4, 6]				# Diagonal
+	]
+	
+	# Check for X wins
+	for condition in win_conditions:
+		if array_x[condition[0]].is_placed and array_x[condition[1]].is_placed and array_x[condition[2]].is_placed:
+			print(x_win)
+			return	# Exit the function if X wins
+			
+	# Check for O wins
+	for condition in win_conditions:
+		if array_o[condition[0]].is_placed and array_o[condition[1]].is_placed and array_o[condition[2]].is_placed:
+			print(o_win)
+			return # Exit the function if O wins
