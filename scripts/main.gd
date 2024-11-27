59588a1491148d27a365c42f6ab1b47c214ba25d
PJC95 <pjconnolly95@pm.me>
2024-11-27 15:48:48 +0000

    Counters now display which symbol they correspond to

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@onready var p1_label = $Labels/P1Label
+@onready var p2_label = $Labels/P2Label
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	p1_label.text = "Player 1 (" + str(GameManager.player_1) + ")"
+	p2_label.text = "Player 2 (" + str(GameManager.player_2) + ")"
+	
 	GameManager.good_theme()
